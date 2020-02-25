d285b2b5abf971357a2a1e1578cc9d24cfca389b
Joseph Kempster <jkempster34@gmail.com>
2020-02-25 12:43:48 +0000

    Add listing 2-3 and update listing 2-2

--- CreateAndUseTypes/Listing_2_2Immutable_DateTime.cs
+++ CreateAndUseTypes/Listing_2_2Immutable_DateTime.cs
@@ -9,6 +9,7 @@ namespace CreateAndUseTypes
 
         /* 
          * DateTime is a structure.
+         * DateTime is immutable, once you have created an instance of DateTime, you cannot change any elements of that instance.
          * AddDays does not change the contents of a DateTime instance, instead it returns a new System.DateTime
          * that adds the specified number of days to value of this new instance.
          */
