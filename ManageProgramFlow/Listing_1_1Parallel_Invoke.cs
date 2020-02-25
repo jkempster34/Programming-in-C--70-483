56f08aa98b92d8d7605e0384fc7afa3b978c6338
Joseph Kempster <jkempster34@gmail.com>
2020-02-25 11:01:12 +0000

    Add project structure and first manage program flow examples

---
+++ ManageProgramFlow/Listing_1_1Parallel_Invoke.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ManageProgramFlow
+{
+    class Listing_1_1Parallel_Invoke
+    {
+
+        /*
+         * Parallel Invoke method is used to launch multiple tasks that are going to be executed in parallel.
+         * Parallel.Invoke accepts a number of Action delegates and creates a task for each of them.
+         * These are replaced with lambdas in this example.
+         * Runtime handles all of the scheduling details.
+         */
+
+        static void Task1()
+        {
+            Console.WriteLine("Task 1 starting");
+            Thread.Sleep(2000);
+            Console.WriteLine("Task 1 ending");
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine("Task 2 starting");
+            Thread.Sleep(1000);
+            Console.WriteLine("Task 2 ending");
+        }
+
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(() => Task1(), () => Task2());
+            Console.WriteLine("Finished processing. Press key to end.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ManageProgramFlow/Listing_1_2Parallel_ForEach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ManageProgramFlow
+{
+    class Listing_1_2Parallel_ForEach
+    {
+        static void WorkOnItem(object item)
+        {
+            Console.WriteLine("Started working on: " + item);
+            Thread.Sleep(100);
+            Console.WriteLine("Finished working on: " + item);
+        }
+
+        static void Main(string[] args)
+        {
+            var items = Enumerable.Range(0, 500);
+            Parallel.ForEach(items, item => WorkOnItem(item));
+        }
+    }
+}
