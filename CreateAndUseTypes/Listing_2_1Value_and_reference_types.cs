bc1b18db9930870b0a1383227dcb35becaca974c
Joseph Kempster <jkempster34@gmail.com>
2020-02-25 11:44:50 +0000

    Update listing 2-1

--- CreateAndUseTypes/Listing_2_1Value_and_reference_types.cs
+++ CreateAndUseTypes/Listing_2_1Value_and_reference_types.cs
@@ -8,11 +8,17 @@ namespace CreateAndUseTypes
     {
 
         /*
-         * Struct is a value type whereas Class is a reference type
-         * In the following example 
+         * Struct is a value type whereas Class is a reference type.
+         * When a struct is assigned to a new variable, all the data is copied, and any 
+         * modification to the new copy does not change the data for the original copy.
+         * In the following example we create an instance of a structure and then assign that instance 
+         * to another variable, the instance is copied, and we end up with two instances of the same structure.
+         * On the other hand, because class variables are managed by reference, both xClass and yClass refer
+         * to the same object. So, changing data in one will affect the data refered to in the other.
          * 
-         * 
-         * 
+         * Example of value type is DateTime. Example of reference type is Bitmap. Because a bitmap image can be
+         * millions of pixels, copying a bitmap if it was a value type would be demanding. Instead, becuase it is managed 
+         * by reference, copying simply involves a copy of the reference.
          */
 
         struct StructStore
