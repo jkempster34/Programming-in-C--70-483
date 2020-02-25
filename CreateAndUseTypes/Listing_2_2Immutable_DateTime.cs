98a2bbf06a2f995ea06ba2427b15234ac5b05c77
Joseph Kempster <jkempster34@gmail.com>
2020-02-25 11:56:11 +0000

    Add listing 2-2

---
+++ CreateAndUseTypes/Listing_2_2Immutable_DateTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateAndUseTypes
+{
+    class Listing_2_2Immutable_DateTime
+    {
+
+        /* 
+         * DateTime is a structure.
+         * AddDays does not change the contents of a DateTime instance, instead it returns a new System.DateTime
+         * that adds the specified number of days to value of this new instance.
+         */
+
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime();
+            date = date.AddDays(1);
+
+            Console.WriteLine(date.ToString());
+        }
+    }
+}
