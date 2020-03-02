e069a9545a7b4bf9bf0401468a7830f812e08a9f
Joseph Kempster <jkempster34@gmail.com>
2020-03-02 10:47:56 +0000

    Update listing 2-5

--- CreateAndUseTypes/Listing_2_5Creating_a_reference.cs
+++ CreateAndUseTypes/Listing_2_5Creating_a_reference.cs
@@ -6,11 +6,11 @@ namespace CreateAndUseTypes
     /*
      * Value type variables are stored on the stack, an area of memory which is allocated 
      * and removed as the program enters and leaves blocks. A value type variable created during the
-     * execuation of a black is stored on the local stack frame and then the entire stack frame is 
-     * discarded when the block completes.
+     * execution of a block is stored on the local stack frame and then the entire stack frame is 
+     * discarded when the block completes (value types have scope).
      * 
      * On the other hand, Reference type variables are allocated memory on a different structure, called the
-     * heap. The heap is managed for an entire application. It is required beause references may be passed 
+     * heap. The heap is managed for an entire application. It is required because references may be passed 
      * between method calls as parameters. Objects can only be removed from the heap when garbage collection
      * determines that there are no references to them.
      */
