3791c7e3fb8f92dac591c8ebf1842119499b8544
Joseph Kempster <jkempster34@gmail.com>
2020-03-02 11:48:02 +0000

    Update listing 2-6

--- CreateAndUseTypes/Listing_2_6_Using_Generic_Types.cs
+++ CreateAndUseTypes/Listing_2_6_Using_Generic_Types.cs
@@ -1,9 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CreateAndUseTypes
 {
+
+    /*
+     * Generics, often given the name T, are used extensively in C# collections. In the example below, 
+     * a stack/reference type (MyStack) is declared which can hold a stack of any type of object. 
+     * Generic classes like this encapsulate operations that are not specific to a particular data type.
+     * When MyStack is instantiated, the desired type is specified, in this case a string.
+     * 
+     * As it is below, Mystack can hold any data type. You can however restrict it to only hold certain types. For example,
+     * if declared as: class MyStack<T> where T:class, only reference types will be able to be stored, in this case it would 
+     * not be possible to store a value type such as an integer. This is refered to as contraints and there are many others.
+     */
+
     class Listing_2_6_Using_Generic_Types
     {
         class MyStack<T>
