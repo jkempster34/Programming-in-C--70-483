36197cdbf7c42a4511256b8d9729a58f9077bb21
Joseph Kempster <jkempster34@gmail.com>
2020-03-02 13:46:30 +0000

    Add listing 2-8

---
+++ CreateAndUseTypes/Listing_2_8_Overloaded_constructors.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CreateAndUseTypes
+{
+    class Listing_2_8_Overloaded_constructors
+    {
+
+        /*
+         * Constructors can have access modifiers, If the constructor is private, the object cannot eb instantiated unless there is a 
+         * public factory method. 
+         * Constructors can be overloaded, so that an object can have multiple version of a constuctor with different signatures.
+         * In the example below, depending on which constructor is called, an Alien will have a particular number of lives, or a 
+         * value of three lives.
+         */
+
+        class Alien
+        {
+            public int X;
+            public int Y;
+            public int Lives;
+
+            public Alien(int x, int y, int lives)
+            {
+                if (x < 0 || y < 0)
+                    throw new ArgumentOutOfRangeException("Invalid position");
+                X = x;
+                Y = y;
+                Lives = lives;
+            }
+
+            public Alien(int x, int y)
+            {
+                X = x;
+                Y = y;
+                Lives = 3;
+            }
+        }
+    }
+}
