743bf6c0382960e0f556b97c9629e7f4965969d8
Joseph Kempster <jkempster34@gmail.com>
2020-03-02 16:35:28 +0000

    Add listing 2-13 and add listing 2-14

---
+++ CreateAndUseTypes/Listing_2_13_Extension_method.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CreateAndUseTypes
+{
+
+    /*
+     * Extension methods allow you to "add" methods to an existing class without needing to create a new derived 
+     * type, or otherwise modify the existing type
+     * 
+     * In the example below, we create an extension method to the String class. The first parameter of the
+     * method specifies the type that the method should be added to, using the keyword 'this' followed by the
+     * name of the type.
+     * 
+     * The extenison method below is called LineCount and counts the number of lines in a string, returning the 
+     * count as an integer. 
+     * 
+     * Once the extension method is created, it can be used in from the namespace in which the class containing 
+     * the method is declared. or by importing the namespace. So, String.LineCount() will only be available in 
+     * namespace CreateAndUseTypes.
+     */
+
+    static class Listing_2_13_Extension_method
+    {
+        public static int LineCount(this String str)
+        {
+            return str.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        static void Main(string[] args)
+        {
+            string text = @"A rocket explorer called Wright,
+                            Once travlled much faster than light,
+                            He set out one day,
+                            In a relative way,
+                            And returned on the previous night";
+
+            Console.WriteLine(text.LineCount());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CreateAndUseTypes/Listing_2_14_Named_parameters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CreateAndUseTypes
+{
+
+    /* 
+     * If you want to call methods without any concern over the order of parameters, you can use named
+     * parameters. 
+     * 
+     * When using named parameters, the compiler uses the name of each argument rather than the position, 
+     * so they can be entered into the method call in any order. This is also good for documentation, when
+     * reading the code, you can see the meaining of each parameter.
+     * 
+     * Ideally, named parameters should be used for any method with more than one argument. 
+     */
+
+    class Listing_2_14_Named_parameters
+    {
+        static void ReadValue(int low, int high, string prompt)
+        {
+            Console.WriteLine("Low is: {0}, high is: {1}, prompt is {2}", low, high, prompt);
+        }
+
+        static void Main(string[] args)
+        {
+            ReadValue(1, 2, "Enter age"); // Not using name parameters
+            ReadValue(low: 1, high: 2, prompt: "Enter age"); // Using named parameters in order
+            ReadValue(prompt: "Enter age", low: 1, high: 2); // Using name parameters out of order
+        }
+    }
+}
