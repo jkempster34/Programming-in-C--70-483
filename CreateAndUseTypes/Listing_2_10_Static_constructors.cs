1e6d1e3fc45e6c12f10c88f2f956c6c59e7e74f5
Joseph Kempster <jkempster34@gmail.com>
2020-03-02 14:33:47 +0000

    Update listing 2-10 and update listing 1-1

--- CreateAndUseTypes/Listing_2_10_Static_constructors.cs
+++ CreateAndUseTypes/Listing_2_10_Static_constructors.cs
@@ -7,6 +7,9 @@ namespace CreateAndUseTypes
      * Static constructors are called once before the creation of the very first instance of the
      * class. The message below is called before the first Alien was created, and not called 
      * when the second alien is created.
+     * 
+     * This is a good place to load resources and initialise values that will be used by instances 
+     * of the class. 
      */
 
     class Listing_2_10_Static_constructors
