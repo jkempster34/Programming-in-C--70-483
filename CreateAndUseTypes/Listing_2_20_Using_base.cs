dbd0b1717208b340ae1857a5ff8a6ef51534ed66
Joseph Kempster <jkempster34@gmail.com>
2020-03-03 13:53:47 +0000

    Add listing 2-20

---
+++ CreateAndUseTypes/Listing_2_20_Using_base.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CreateAndUseTypes
+{
+    class Listing_2_20_Using_base
+    {
+
+        /*
+         * The base keyword is used to access members of the base class from within a derived class. 
+         * 
+         * In the example below, we call a method on the base class (DoPrint) that has been overridden by another 
+         * method.
+         */
+
+        class Document
+        {
+            public void GetDate()
+            {
+                Console.WriteLine("Hello from GetDate in the Document");
+            }
+
+            public virtual void DoPrint()
+            {
+                Console.WriteLine("Hello from DoPrint in Document");
+            }
+        }
+
+        class Invoice : Document
+        {
+            public override void DoPrint()
+            {
+                Console.WriteLine("Hello from DoPrint in Invoice");
+            }
+        }
+
+        class PrePaidInvoice : Invoice
+        {
+            public override void DoPrint()
+            {
+                base.DoPrint();
+                Console.WriteLine("Hello from DoPrint in PrePaidInvoice");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrePaidInvoice p = new PrePaidInvoice();
+            p.GetDate();
+            p.DoPrint();
+
+            Console.ReadKey();
+        }
+    }
+}
