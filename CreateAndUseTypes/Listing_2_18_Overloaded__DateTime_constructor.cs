ff14942134247fea63f9d48dc3b384af778a40f6
Joseph Kempster <jkempster34@gmail.com>
2020-03-03 13:26:05 +0000

    Add listing 2-18

---
+++ CreateAndUseTypes/Listing_2_18_Overloaded__DateTime_constructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CreateAndUseTypes
+{
+
+    /*
+     * Overloading means to provide a method with the same name, but a different signature. 
+     * 
+     * DateTime has a large number of overloaded constructors as there are many ways a programmer may want to initialise a
+     * DateTime value. Two examples can be seen below. 
+     */
+
+    class Listing_2_18_Overloaded__DateTime_constructor
+    {
+        static void Main(string[] args)
+        {
+            DateTime d0 = new DateTime(ticks: 636679008000000000);
+            DateTime d1 = new DateTime(year: 2018, month: 7, day: 23);
+            Console.WriteLine(d0);
+            Console.WriteLine(d1);
+            Console.ReadKey();
+        }
+    }
+}
